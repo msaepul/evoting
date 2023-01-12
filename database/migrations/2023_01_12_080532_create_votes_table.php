410d7a436ffc419ba081272d05bbbc2231a77d5f
msaepul12 <ilybassar@gmail.com>
2023-01-12 16:22:42 +0700

    add sc

---
+++ database/migrations/2023_01_12_080532_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_vot');
+            $table->string('produk');
+            $table->string('id_penilaian');
+            $table->string('nilai');
+            $table->string('input_harga');
+            $table->string('masukan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+};
